ab8234d754a9092c3ed40eab87a1c2e5d6db19af
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-04-29 12:30:09 -0400

    Update1.0.0

--- example.py
+++ example.py
@@ -2,3 +2,11 @@ def example(any):
     return type(any)
 
 print(example(True))
+
+def sayMsg(msg):
+    if isinstance(msg,(str)) == False:
+        return "not is string!"
+    else:
+        return msg
+
+print(sayMsg(True))
