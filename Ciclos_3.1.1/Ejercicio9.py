e8e68a84668d8fd68115c80d6b81fd7060b0b3f8
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-05-10 14:34:26 -0400

    Ejericicios 3.1.1 10/05/2022  (editado)

--- Ciclos_3.1.1/Ejercicio9.py
+++ Ciclos_3.1.1/Ejercicio9.py
@@ -1,12 +1,8 @@
-from __future__ import barry_as_FLUFL
-
-
 while True:
     total = 0
-    val = False
     n = int(input("Numero: "))
     if n <= 0:
-        print("Numero tiene que ser mayor a 0")
+        print("Numero ti que ser mayor a 0")
     else:
         for i in range(1, n):
             if n % i == 0:
