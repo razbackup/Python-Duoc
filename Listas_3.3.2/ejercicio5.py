62e7a195d5c431b7766d08d5efc4b60eb21e3a30
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-05-17 15:50:02 -0400

    Ejericicios 3.3.2 17/05/2022 (fix)

--- Listas_3.3.2/ejercicio5.py
+++ Listas_3.3.2/ejercicio5.py
@@ -1,10 +1,16 @@
 names = list()
 lastNames = list()
-for i in range(1,4):
-    name = str(input(f"Nombre {i}: "))
-    lastName = str(input(f"Apellido {i}: "))
+sortNames = []
+for i in range(3):
+    name = str(input(f"Nombre {i+1}: ")).capitalize()
+    lastName = str(input(f"Apellido {i+1}: ")).capitalize()
     names.append(name)
     lastNames.append(lastName)
+    auxComplete = f"{name} {lastName}"
+    sortNames.append(auxComplete) 
 lastNames.sort()
 names.sort()
+sortNames.sort()
 print(f"Nombres: {names} Apellidos: {lastNames}")
+for i in sortNames:
+    print(i)
