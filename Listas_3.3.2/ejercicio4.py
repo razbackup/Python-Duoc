9a9fd43673f01aa5a49ddd83ed9a41d4862d3592
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-05-17 15:27:31 -0400

    Ejericicios 3.3.2 17/05/2022 (fix)

--- Listas_3.3.2/ejercicio4.py
+++ Listas_3.3.2/ejercicio4.py
@@ -4,7 +4,7 @@ for i in range(1,4):
     name = str(input(f"Nombre {i}: "))
     arr.append(name)
 for i in arr:
-    if i > aux:
+    if len(i) > len(aux):
         aux = i
 
 print(f"Nombre mas grande es: {aux}")
