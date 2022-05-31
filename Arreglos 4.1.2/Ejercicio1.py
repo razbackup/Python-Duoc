29d11ad44585d694e4247e29c5dd35a1b448ca15
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-05-31 14:54:58 -0400

    31/05/2022

--- Arreglos 4.1.2/Ejercicio1.py
+++ Arreglos 4.1.2/Ejercicio1.py
@@ -1,4 +1,13 @@
 from numpy import *
 from random import randint as rd
-arregloA = array([rd(0,500) for i in range(1,100)])
-indexs = [i for i in arregloA if i%2 == 0]
+arregloA = array([rd(0,500) for i in range(1,101)])
+arregloPar = [i for i in arregloA if i%2 == 0]
+print(f"Elementos Pares: {arregloPar}")
+print(f"Elemento Maximo: {arregloA.max()}")
+print(f"Elemento Posicion Max: {max(where(arregloA == arregloA.max())[0])}")
+print(f"Elemento Menor: {arregloA.min()}")
+arregloB = arregloA.copy()
+print(f" Elementos multiplicados por tres {arregloB * 3}")
+print(f"Suma de los elementos: {arregloB.sum()}")
+print(f"Promedio de los elementos: {round(arregloB.mean(),2)}")
+
