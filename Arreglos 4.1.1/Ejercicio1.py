5967943fc665fd257cbb765bf93a1f7b6634be4a
GodDoesNotPlayDice <isaacgamerhd95@gmail..com>
2022-05-31 14:14:37 -0400

    31/05/2022

--- Arreglos 4.1.1/Ejercicio1.py
+++ Arreglos 4.1.1/Ejercicio1.py
@@ -1,10 +1,10 @@
-import numpy as np
-import random as r
-arr = np.array([10])
-lista = list()
-cont = 0
-for i in arr:
-    while cont <= int(i):
-        nArr = np.append(arr,r.randint(1,10))
-        print(nArr)
-        cont+=1
+from numpy import *
+from random import randint as rn
+arr = array([rn(0,100) for i in range(1,11)])
+arr2 = arr[:].copy()
+print(f"elemento mayor: {arr2.max()}")
+print(f"elemento menor: {arr2.min()}")
+print(f"Suma de elementos: {arr2.sum()}")
+print(f"Promedio de los elementos: {arr2.mean()}")
+print(f"Elementos: {[i for i in arr2]}")
+
